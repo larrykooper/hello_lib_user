a103b413258e6163e4a48b13fed513d69d5e9e6e
Larry Kooper <larry@605.tv>
2017-11-13 15:35:36 -0500

    First working version, publishing the lib locally

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "helloLibUser"
+
+version := "0.1"
+
+scalaVersion := "2.12.3"
+
+libraryDependencies += "tv.sixzerofive"      % "hello_library_2.12"   % "0.0.1-SNAPSHOT"
---
+++ src/main/scala/tv/sixzerofive/HelloLibUser.scala
@@ -0,0 +1,12 @@
+import tv.sixzerofive.hello_library.hello_library._
+
+object HelloLibUser {
+
+  def main(args: Array[String]) {
+    val name="Robert Winston"
+    //tv.sixzerofive.hello_library.sayHello(name)
+    val s = sayHello(name)
+    println(s)
+
+  }
+}
